47298e816d308c83d1589e9265bcaf88f8f5acff
Hritesh_Saha <hriteshsaha4@gmail.com>
2024-08-19 17:53:12 +0530

    try 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,9 @@ import io from 'socket.io-client'
 import { useState } from 'react';
 import Chat from './Chat';
 
-const socket=io.connect("https://vibe-bridge-server.vercel.app/");
+const socket=io.connect("https://vibe-bridge-server.vercel.app/",{
+  transports: ["websocket"]
+});
 
 function App() {
  const [form,setForm]=useState({username:"",room:""});
