8d78afa1d09405f49ab12deb7625004b85b1bed6
Hritesh_Saha <hriteshsaha4@gmail.com>
2024-08-19 17:22:54 +0530

    adding vercel.json

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const server=http.createServer(app);
 
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:3000",
+        origin:"https://vibe-bridge-client.vercel.app/",
         methods:["GET","POST"],
     }
 })
